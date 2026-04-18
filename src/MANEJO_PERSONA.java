cce41441fa8391357dedd4920b38a062719172a4
sofiachablepad11-cpu <sofiachablepad.11@gmail.com>
2026-04-18 16:27:36 -0600

    Se agregó MANEJO_PERSONA para guardar y ordenar registros en lista doblemente enlazada

---
+++ src/MANEJO_PERSONA.java
@@ -0,0 +1,154 @@
+public class MANEJO_PERSONA {
+
+    private PERSONA primero, ultimo = null;
+
+    
+    public void insertarNodoFinal(PERSONA nuevo) {
+        if (primero == null) {
+            primero = ultimo = nuevo;
+        } else {
+            ultimo.siguiente = nuevo;
+            nuevo.anterior = ultimo;
+            ultimo = nuevo;
+        }
+    }
+
+    
+    public void agregarAlInicio(PERSONA nuevo) {
+        if (primero == null) {
+            primero = ultimo = nuevo;
+        } else {
+            nuevo.siguiente = primero;
+            primero.anterior = nuevo;
+            primero = nuevo;
+        }
+    }
+
+    
+    public void agregarEnMedio(PERSONA nuevo) {
+
+        PERSONA aux = primero;
+
+        while (aux != null) {
+
+            if (nuevo.getNombre().compareToIgnoreCase(aux.getNombre()) < 0) {
+
+                nuevo.siguiente = aux;
+                nuevo.anterior = aux.anterior;
+
+                aux.anterior.siguiente = nuevo;
+                aux.anterior = nuevo;
+
+                return;
+            }
+
+            aux = aux.getSiguiente();
+        }
+    }
+
+    
+    public boolean insertarOrdenado(String nombre, int valor, int dia, int mes, int año) {
+
+        PERSONA nuevo = new PERSONA(nombre, valor, dia, mes, año);
+
+        
+        PERSONA aux = primero;
+
+        while (aux != null) {
+            if (aux.getNombre().equalsIgnoreCase(nombre)) {
+                return false;
+            }
+            aux = aux.getSiguiente();
+        }
+
+        
+        if (primero == null) {
+            primero = ultimo = nuevo;
+        }
+
+        
+        else if (nombre.compareToIgnoreCase(primero.getNombre()) < 0) {
+            agregarAlInicio(nuevo);
+        }
+
+        
+        else if (nombre.compareToIgnoreCase(ultimo.getNombre()) > 0) {
+            insertarNodoFinal(nuevo);
+        }
+
+        
+        else {
+            agregarEnMedio(nuevo);
+        }
+
+        return true;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
